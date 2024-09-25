f85e1206928edafc7bc0461b73bce65c7e7d9328
PrashanthSai-K <srisathyasai24680@gmail.com>
2024-09-25 11:18:46 +0530

    react paginate updated

--- src/components/Diseases.js
+++ src/components/Diseases.js
@@ -1,172 +1,38 @@
 import React, { useState } from 'react'
 import Navbar from './Navbar'
 import ReactPaginate from 'react-paginate';
+import Vegetables from './Vegetables';
 
 function Diseases() {
 
-    const vegetableDisease = [
-        {
-            name: "Anthracnose (Peppers)",
-            cause: "Anthracnose is caused by a group of fungi within the genus Colletotrichum. The two main species associated with peppers are C. gloeosporioides and C. acutatum, but there are several species that may cause this disease.",
-            image: "/images/vegetables/Anthracnose in Peppers.jpg"
-        },
-        {
-            name: "Bacterial Wilt",
-            cause: "Bacterial wilt is a plant disease caused by specific bacteria that infect a variety of plants, including important crops like tomatoes, potatoes, cucumbers, and many others. The disease is particularly notorious for causing significant yield losses in agriculture.",
-            image: "/images/vegetables/bacterial-wilt.png"
-        },
-        {
-            name: "Black Rot (Cabbage)",
-            cause: "Black Rot is a significant bacterial disease affecting cabbage and other cruciferous crops. It is caused primarily by the bacterium Xanthomonas campestris pv. campestris. This disease can lead to severe yield losses and is particularly detrimental in humid conditions.",
-            image: "/images/vegetables/Black Rot in Cabbage.jpg"
-        },
-        {
-            name: "Cercospora Leaf Spot",
-            cause: "Cercospora Leaf Spot is a common fungal disease that affects a variety of plants, including crops such as beans, cucumbers, and spinach, as well as ornamental plants. It is caused by fungi of the genus Cercospora. The disease can lead to reduced photosynthesis and overall plant vigor, resulting in significant yield losses.",
-            image: "/images/vegetables/Cercospora Leaf Spot in Spinach.jpg"
-        },
-        {
-            name: "Clubroot (Cabbage)",
-            cause: "Clubroot is a serious soil-borne disease that affects cabbage and other cruciferous crops, including broccoli, cauliflower, and Brussels sprouts. It is caused by the pathogen Plasmodiophora brassicae, a type of protist. The disease can lead to significant yield losses and is particularly problematic in cool, wet conditions.",
-            image: "/images/vegetables/Clubroot (Cabbage and Broccoli).jpg"
-        },
-        {
-            name: "Damping Off (Seedlings)",
-            cause: "ADamping off is a term used to describe a complex of diseases that affect seedlings, leading to their decay and death shortly after germination. It is caused by a variety of fungal pathogens, including those from the genera Pythium, Rhizoctonia, and Botrytis. This disease is especially common in greenhouse settings and can severely impact seedling production.",
-            image: "/images/vegetables/Damping Off (Seedlings).png"
-        },
-        {
-            name: "Downy Mildew (Lettuce)",
-            cause: "Downy mildew is a significant fungal disease affecting lettuce and other leafy greens, characterized by the presence of downy, grayish-white fungal growth on the underside of leaves. The disease is caused primarily by the pathogen Bremia lactucae, which can lead to severe crop losses and is particularly problematic in cool, humid conditions.",
-            image: "/images/vegetables/Downy Mildew on Lettuce.jpg"
-        },
-        {
-            name: "Fusarium Wilt (Tomatoes)",
-            cause: "Fusarium wilt is a serious soil-borne disease that affects tomato plants, caused by the fungus Fusarium oxysporum f.sp. lycopersici. This disease can lead to significant yield losses and is particularly challenging to manage due to its persistence in the soil.",
-            image: "/images/vegetables/Fusarium Wilt (Tomatoes).jpg"
-        },
-        {
-            name: "Late Blight (Potatoes)",
-            cause: "Late blight is a devastating disease affecting potato and tomato plants, primarily caused by the oomycete pathogen Phytophthora infestans. This disease is notorious for its ability to cause rapid crop losses and is often associated with cool, moist conditions.",
-            image: "/images/vegetables/Late Blight (Potatoes).jpg"
-        },
-        {
-            name: "Leaf Spot (Spinach)",
-            cause: "Leaf spot is a common term used to describe various diseases affecting spinach, characterized by the presence of spots or lesions on the leaves. The disease can be caused by several fungal and bacterial pathogens, with the most notable being Cercospora beticola and Alternaria species. Leaf spot diseases can lead to reduced crop quality and yield if not managed effectively.",
-            image: "/images/vegetables/Leaf Spot in Spinach.jpg"
-        },
-        {
-            name: "Onion Smut",
-            cause: "Onion smut is a fungal disease caused by the pathogen Urocystis cepulae, affecting onions and related allium crops. This disease is characterized by the formation of dark, smutty galls on bulbs, leaves, and flowers, leading to significant crop losses if not managed properly.",
-            image: "/images/vegetables/Onion Smut.jpg"
-        },
-        {
-            name: "Powdery Mildew (Cucurbits)",
-            cause: "Powdery mildew is a common fungal disease that affects a wide range of cucurbit crops, including cucumbers, squash, pumpkins, and melons. It is caused by several species of fungi, most notably Podosphaera xanthii and Erysiphe cichoracearum. The disease is characterized by the appearance of white, powdery fungal growth on the leaves and stems, which can lead to significant crop losses if not managed effectively.",
-            image: "/images/vegetables/Powdery Mildew (Cucurbits).jpg"
-        },
-        {
-            name: "Pythium Root Rot (Cucumber)",
-            cause: "Pythium root rot is a serious disease affecting cucumber plants, caused by various species of the water mold genus Pythium. This disease can lead to significant yield losses, especially in conditions where the soil is overly saturated and poorly drained.",
-            image: "/images/vegetables/Pythium Root Rot (Cucumber).jpeg"
-        },
-        {
-            name: "Root Knot Nematodes",
-            cause: "Root knot nematodes (RKN), particularly species of the genus Meloidogyne, are parasitic roundworms that cause significant damage to a wide range of crops, including vegetables, fruits, and ornamentals. These nematodes are known for inducing characteristic galls or knots on the roots, which can severely affect plant health and yield.",
-            image: "/images/vegetables/root knot nematodes.png"
-        },
-        {
-            name: "White Mold (Beans)",
-            cause: "White mold, also known as sclerotinia stem rot, is a fungal disease caused by Sclerotinia sclerotiorum. This disease affects various crops, particularly beans, and can lead to significant yield losses if not managed properly. White mold is characterized by the presence of white, fluffy fungal growth and the formation of sclerotia, which are hard, black structures that can survive in the soil for years.",
-            image: "/images/vegetables/Pythium Root Rot (Cucumber).jpeg"
-        },
-    ]
-
-    const [currentPage, setCurrentPage] = useState(0);
-    const diseasesPerPage = 8;
-
-    // Calculate the number of pages
-    const pageCount = Math.ceil(vegetableDisease.length / diseasesPerPage);
-
-    // Get the current diseases based on the current page
-    const indexOfFirstDisease = currentPage * diseasesPerPage;
-    const currentDiseases = vegetableDisease.slice(indexOfFirstDisease, indexOfFirstDisease + diseasesPerPage);
-
-    // Handle page click
-    const handlePageClick = (data) => {
-        setCurrentPage(data.selected);
-    };
+    const [page, setPage] = useState("vegetables");
 
     return (
         <>
             <Navbar />
-            <h1 className='text-emerald-1000 text-2xl font-semibold pl-20 pt-4' >DISEASES</h1>
-            <h1 className=' text-xl text-emerald-1000 font-semibold pl-20 pt-4 '>VEGETABLES</h1>
-
-            {/* <div className='px-1 py-2 flex gap-5 flex-wrap h-auto bg-emerald-50 w-full items-center justify-center pt-10'>
-                {vegetableDisease.map((vege) => {
-                    return (
-                        <div className='w-64 h-80 bg-white rounded-2xl overflow-hidden p-3 border border-dashed border-emerald-1000 shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-                            <img className='w-full h-1/2 object-cover rounded-2xl' src={vege.image} alt="" />
-                            <div>
-                                <h1 className='text-emerald-1000 text-xl font-medium' title={vege.name}>{vege.name.length > 19 ? `${vege.name.slice(0, 18)}...` : vege.name}</h1>
-                                <p className='text-xs p-1 text-justify'>{vege.cause.length > 230 ? `${vege.cause.slice(0, 230)}...` : vege.cause}</p>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div> */}
-            <div className='flex items-center justify-center'>
-                <div className='px-1 py-2 flex flex-col gap-5 flex-wrap h-auto bg-emerald-50 w-7/12 items-center justify-center pt-10'>
-                    <div className='flex gap-5 flex-wrap items-center justify-center'>
-                        {currentDiseases.map((vege) => (
-                            <div key={vege.name} className='w-64 h-80 bg-white rounded-2xl overflow-hidden p-3 border border-dashed border-emerald-1000 shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-                                <img className='w-full h-1/2 object-cover rounded-2xl' src={vege.image} alt={vege.name} />
-                                <div>
-                                    <h1 className='text-emerald-1000 text-xl font-medium' title={vege.name}>
-                                        {vege.name.length > 19 ? `${vege.name.slice(0, 18)}...` : vege.name}
-                                    </h1>
-                                    <p className='text-xs p-1 text-justify'>
-                                        {vege.cause.length > 230 ? `${vege.cause.slice(0, 230)}...` : vege.cause}
-                                    </p>
-                                </div>
-                            </div>
-                        ))}
+            <div className='flex'>
+                <h1 className='text-emerald-1000 text-2xl font-semibold pl-20 pt-4' >DISEASES</h1>
+                <div class="relative inline-block text-left">
+                    <div>
+                        <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                            Options
+                            <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
                     </div>
-
-                    {/* React Paginate */}
-                    <div className='pt-5'>
-                        {/* <ReactPaginate
-                        previousLabel={'Previous'}
-                        nextLabel={'Next'}
-                        breakLabel={'...'}
-                        breakClassName={'break-me'}
-                        pageCount={pageCount}
-                        marginPagesDisplayed={2}
-                        pageRangeDisplayed={5}
-                        onPageChange={handlePageClick}
-                        containerClassName={'pagination'}
-                        activeClassName={'active'}
-                        pageClassName={'px-3 py-1 bg-emerald-500 text-white rounded mx-1'}
-                        previousClassName={'px-3 py-1 bg-emerald-500 text-white rounded mx-1'}
-                        nextClassName={'px-3 py-1 bg-emerald-500 text-white rounded mx-1'}
-                        activeLinkClassName={'p-1 rounded-full bg-emerald-1000'}
-                        disabledClassName={'opacity-50 cursor-not-allowed'}
-                    /> */}
-
-                        <ReactPaginate
-                            breakLabel="..."
-                            nextLabel="next >"
-                            onPageChange={handlePageClick}
-                            pageRangeDisplayed={5}
-                            pageCount={pageCount}
-                            containerClassName='flex gap-5 '
-                            previousLabel="< previous"
-                            renderOnZeroPageCount={null}
-                        />                </div>
+                    <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                        <div class="py-1" role="none">
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">Vegetables</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Fruits</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
 
+            </div>
+            {page === "vegetables" &&
+                <Vegetables />
+            }
         </>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,11 @@ import Navbar from './Navbar';
 
 function Home() {
 
-    const navigate = useNavigate();
-
     return (
         <>
             <Navbar />
-            <div className=' sm:flex items-center justify-center h-5/6  flex flex-col-reverse sm:pl-20 w-full' >
-                <div className=' sm:pl-24 pl-6 h-1/2 w-full flex flex-col items-center justify-center sm:h-auto text-center pt-8'>
+            <div className=' sm:flex items-center justify-center h-5/6  flex flex-col-reverse  w-full' >
+                <div className='  h-1/2 w-full flex flex-col items-center justify-center sm:h-auto text-center pt-8'>
                     <h1 className='drop-shadow-2xl text-emerald-950 sm:text-6xl font-medium text-4xl '>
                         WELCOME !!!
                     </h1>
@@ -19,7 +17,7 @@ function Home() {
                     </h1>
                     <p className='text-xl text-emerald-900 p-3'>Identify common plant diseases and find effective solutions to keep your plants healthy.</p>
                 </div>
-                <div className=' flex items-end w-full sm:justify-center justify-center sm:w-1/2 w-full sm:h-auto h-1/2 '>
+                <div className=' flex items-end sm:justify-center justify-center sm:w-1/2 w-full sm:h-auto h-1/2  '>
                     <img className=' drop-shadow-2xl w-80 sm:w-3/6' src="/images/PlantImage.png" alt="" />
                 </div>
             </div>
